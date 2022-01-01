e023755e2cbf0027e79c22dd9590e2b31617c099
Mamun Mahmood <mamamun1999@gmail.com>
2022-01-02 03:22:02 +0600

    layer box update

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const addVarriation = (id) => {
   const mainDiv = document.getElementById("new_div");
   const subDiv = document.createElement("div");
   const newDiv = `<div id="varriation_div">
-  <div id="layer_container" class="container layer_div p-3 w-75 mt-3 rounded">
+  <div id="layer_container${btnCount -1}" class="container layer_div p-3 w-75 mt-3 rounded">
   <h5 style="text-align: center;">Varriation ${btnCount -1}</h5>
     <div class="display_flex_style">
       <div>
@@ -16,7 +16,7 @@ const addVarriation = (id) => {
           src="https://img.icons8.com/ios/60/000000/long-arrow-right.png"
         />
       </div>
-      <div><input id="layer_img" type="file" onchange="addLayer(2)" /></div>
+      <div><input id="layer_img" type="file" onchange="addLayer(${btnCount})" /></div>
     </div>
   </div>
   <div style="text-align: center;">
@@ -35,7 +35,7 @@ const addVarriation = (id) => {
 let layerCount = 1;
 const addLayer = (id) => {
   layerCount++;
-  const mainDiv = document.getElementById(`layer_container`);
+  const mainDiv = document.getElementById(`layer_container${id-1}`);
   const subDiv = document.createElement("div");
   const newDiv = `<div class="display_flex_style">
   <div>
@@ -46,7 +46,7 @@ const addLayer = (id) => {
       src="https://img.icons8.com/ios/60/000000/long-arrow-right.png"
     />
   </div>
-  <div><input id="layer_img" onchange="addLayer()" type="file" /></div>
+  <div><input id="layer_img" onchange="addLayer(${btnCount})" type="file" /></div>
 </div>`;
   subDiv.innerHTML = newDiv;
   mainDiv.appendChild(subDiv);
