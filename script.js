1087018878f774fbaab670d2d0c9f17f839ade0a
Mamun Mahmood <mamamun1999@gmail.com>
2022-01-02 01:55:00 +0600

    add varriation btn hiding after clicked

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-const addVarriation = () => {
-  const varriationBtn = document.getElementById("varriation_btn");
-  varriationBtn.style.display = "none";
+let btnCount = 2;
+const addVarriation = (id) => {
+  btnCount++;
+  document.getElementById(id).style.display = "none";
   const mainDiv = document.getElementById("new_div");
   const subDiv = document.createElement("div");
   const newDiv = `<div id="varriation_div">
-    <div class="container layer_div p-3 w-75">
+    <div class="container layer_div p-3 w-75 mt-3 rounded">
     <button class="btn btn-danger m-2">Layer 1</button>
     </div>
     <div style="text-align: center">
-    <button id="varriation_btn" onclick="addVarriation()" class="btn btn-info m-3">
+    <button id="varriation_btn${btnCount}" onclick="addVarriation('varriation_btn${btnCount}')" class="btn btn-info m-3">
     Add Varriation
     </button>
     </div>
