7dcb4daa70af2b5591b37953812e3a0a9ea25d0b
Mamun Mahmood <mamamun1999@gmail.com>
2022-01-05 16:10:22 +0600

    varriation mixed fix

--- script.js
+++ script.js
@@ -5,19 +5,13 @@ const addVarriation = (id) => {
   const mainDiv = document.getElementById("new_div");
   const subDiv = document.createElement("div");
   const newDiv = `<div id="varriation_div">
-  <div id="layer_container${btnCount -1}" class="container layer_div p-3 w-75 mt-3 rounded">
-  <h5 style="text-align: center;">Layer ${btnCount -1}</h5>
-    <div class="display_flex_style">
-      <div>
-        <label for="layer_img" class="btn btn-danger">Add Varriation 1</label>
-      </div>
-      <div>
-        <img
-          src="https://img.icons8.com/ios/60/000000/long-arrow-right.png"
-        />
-      </div>
-      <div><input id="layer_img" type="file" onchange="addLayer(${btnCount})" /></div>
-    </div>
+  <div class="layer_div rounded container w-75 mt-3">
+  <div id="layer_container${
+    btnCount - 1
+  }" class="p-3">
+  <h5 style="text-align: center;">Layer ${btnCount - 1}</h5>
+  </div>
+  <div style="text-align: center"><button id="add_varr_btn" onclick="addLayer(${btnCount})" class="btn btn-success mb-3">Add Varriation</button></div>
   </div>
   <div style="text-align: center;">
     <button
@@ -32,25 +26,29 @@ const addVarriation = (id) => {
   subDiv.innerHTML = newDiv;
   mainDiv.appendChild(subDiv);
 };
-let layerCount = 1;
+let layerCount = 0;
 const addLayer = (id) => {
+  // document.getElementById("add_varr_btn").style.display = "none";
   layerCount++;
-  const mainDiv = document.getElementById(`layer_container${id-1}`);
+  const mainDiv = document.getElementById(`layer_container${id - 1}`);
   const subDiv = document.createElement("div");
-  const newDiv = `<div class="display_flex_style">
+  const newDiv = `<div><div class="display_flex_style">
+  
   <div>
-    <label for="layer_img${layerCount}" class="btn btn-danger">Add Varriation ${layerCount}</label>
+    <label for="layer_img${layerCount}" class="btn btn-danger">Select Varriation</label>
   </div>
   <div>
     <img
       src="https://img.icons8.com/ios/60/000000/long-arrow-right.png"
     />
   </div>
-  <div><input id="layer_img" onchange="addLayer(${btnCount})" type="file" /></div>
+  <div><input id="layer_img" type="file" /></div>
+  </div>
+  
 </div>`;
   subDiv.innerHTML = newDiv;
   mainDiv.appendChild(subDiv);
 };
-document.getElementById('add_layer').addEventListener('click', () => {
-  document.getElementById('varriation_btn_div').style.display = 'block'
-})
+document.getElementById("add_layer").addEventListener("click", () => {
+  document.getElementById("varriation_btn_div").style.display = "block";
+});
